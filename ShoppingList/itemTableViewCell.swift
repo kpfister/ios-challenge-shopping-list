c62d6bf0091294a92b34da7363851daa471a0d95
kpfister <kpfister@me.com>
2016-05-20 09:22:45 -0600

    Set up my view but now im lost..

---
+++ ShoppingList/itemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  itemTableViewCell.swift
+//  ShoppingList
+//
+//  Created by Karl Pfister on 5/20/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class itemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
